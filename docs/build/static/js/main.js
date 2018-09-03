0009bd90d77a192fb8c062939d997d513f859761
Evgeny Vedegis <tichoo@yandex.ru>
2018-09-03 22:52:35 +0300

    Add Alert, Layout

--- docs/build/static/js/main.js
+++ docs/build/static/js/main.js
@@ -1,15 +1,43 @@
+// Sidebar
 var sidebar = document.querySelector('.sidebar');
-var contentWrapper = document.querySelector('.content-wrapper--sidebar');
-var sidebarOpenBtn = document.querySelector('.sidebar__open-buttom .button-burger');
-var sidebarCloseBtn = document.querySelector('.button-close');
+var sidebarBtn = document.querySelector('.sidebar-open-buttom .button-burger');
 
-sidebarOpenBtn.onclick = function(){
-  this.classList.toggle("active");
-  sidebar.classList.toggle("full");
-  if(this.classList.contains('active')){
-    sidebar.style="left: 0;";
-  }else{
-    sidebar.style="left: -240px;";
+if(sidebarBtn){
+  sidebarBtn.onclick = function(){
+    this.classList.toggle('active');
+    sidebar.classList.toggle('full');
+    if(this.classList.contains('active')){
+      sidebar.style='left: 0;';
+    }else{
+      sidebar.style='left: -240px;';
+    }
+  }
+}
+
+// Navbar
+var navbarListMobile = document.querySelector('.navbar-list--mobile');
+var navbarMenuMobileBtn = document.querySelector('.navbar-menu--mobile .button-burger');
+
+if(navbarMenuMobileBtn){
+  navbarMenuMobileBtn.onclick = function(){
+    this.classList.toggle('active');
+    if(this.classList.contains('active')){
+      navbarListMobile.style='display: flex;'
+    }else{
+      navbarListMobile.style='display: none;'
+    }
+  }
+}
+
+// Alert
+var alertUi = document.querySelectorAll('.ui-alert');
+var alertBtnClose = document.querySelectorAll('.ui-alert .button-close');
+
+if(alertUi){
+  for (var i = 0; i < alertBtnClose.length; i++) {
+    alertBtnClose[i].onclick = function(){
+      this.parentNode.parentNode.removeChild(this.parentNode);
+    }
   }
 
 }
