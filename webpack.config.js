d67ec0a6109cf007a0c1577ef3fd656a2e422444
Evgenii Vedegis <tichoo@yandex.ru>
2021-10-10 10:50:39 +0800

    better build with cli

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCSSPlugin = require('mini-css-extract-plugin')
 
 const distFolder = 'dist'
 
 module.exports = {
   entry: {
+    // since 2.0.5 using for dev only
     uimini: path.resolve(__dirname, 'src', 'builds/default.js'),
     uimini_no_reset: path.resolve(__dirname, 'src', 'builds/no-reset.js')
   },
@@ -20,20 +20,11 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          MiniCSSPlugin.loader,
-          // 'style-loader',
+          'style-loader',
           {
             loader: 'css-loader',
             options: {
               sourceMap: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                config: path.resolve(__dirname, 'postcss.config.js')
-              }
             }
           },
           {
@@ -51,9 +42,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'test.html')
-    }),
-    new MiniCSSPlugin({
-      filename: '[name].css'
     })
   ]
 }
