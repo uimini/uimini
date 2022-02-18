0cac8c0c72bbc08eed5e6ae9c3e9118da825286b
Evgenii Vedegis <tichoo@yandex.ru>
2022-02-18 09:16:08 +0800

    added pointer-events (buttons, tags)

--- release-zip.js
+++ release-zip.js
@@ -10,6 +10,6 @@ const pkg = require('./package.json')
 const version = pkg.version
 const distFolder = `dist`
 // path to the doc dir
-const rootDocsDir = '../uimini-new-doc-repo/docs/.vuepress/public/download/'
+const rootDocsDir = '../docs/docs/.vuepress/public/download/'
 
 sh.exec(`zip -r9 "${rootDocsDir}uimini-${version}.zip" "${distFolder}"`)
